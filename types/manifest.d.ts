583ee34bafbaa484b03883a1d53fa3a3fe04e7dd
Arnaud Gissinger <agissing@student.42.fr>
2022-05-28 11:58:25 +0200

    [UPDATE] add screenshots, categories and shortcuts to manifest

--- types/manifest.d.ts
+++ types/manifest.d.ts
@@ -1,5 +1,24 @@
 /* eslint camelcase: 0 */
 
+export type Category = 'books' | 'business' | 'education' | 'entertainment' | 'finance' | 'fitness' | 'food' | 'games' | 'government' | 'health' | 'kids' | 'lifestyle' | 'magazines' | 'medical' | 'music' | 'navigation' | 'news' | 'personalization' | 'photo' | 'politics' | 'productivity' | 'security' | 'shopping' | 'social' | 'sports' | 'travel' | 'utilities' | 'weather';
+export type Platform = 'wide' | 'narrow' | 'android' | 'chromeos' | 'ios' | 'kaios' | 'macos' | 'windows' | 'xbox' | 'chrome_web_store' | 'play' | 'itunes' | 'microsoft-inbox' | 'microsoft-store';
+
+export interface Shortcut {
+  name: string,
+  url: string,
+  short_name?: string,
+  description?: string,
+  icons?: string[],
+}
+
+export interface Screenshot {
+  label?: string,
+  platform?: Platform,
+  src: string,
+  sizes: string,
+  type: string,
+}
+
 export interface ManifestOptions {
   /**
    * Default: _npm_package_name_
@@ -17,6 +36,18 @@ export interface ManifestOptions {
    *
    */
   icons: Record<string, any>[],
+  /**
+   * https://developer.mozilla.org/en-US/docs/Web/Manifest/screenshots
+   */
+  screenshots: Screenshot[],
+  /**
+   * https://developer.mozilla.org/en-US/docs/Web/Manifest/categories
+   */
+  categories: Category[],
+  /**
+   * https://developer.mozilla.org/en-US/docs/Web/Manifest/shortcuts
+   */
+  shortcuts: Shortcut[],
   /**
    * Default: `routerBase + '?standalone=true'`
    */
